250002f27058f84a75beeb747ae12f3c00faaebd
chrismcdermut <chrismcdermut@gmail.com>
2020-06-16 18:03:04 -0700

    not working but saving things

--- Strategies/AuroraBacktestUnlocked.cs
+++ Strategies/AuroraBacktestUnlocked.cs
@@ -37,7 +37,13 @@ namespace NinjaTrader.NinjaScript.Strategies
 		private string labels;
 		private int dataIndex;
 		private DateTime localDate;
-		private string barType;
+		private BarsPeriodType barType;
+		private int barValue;
+		private string strategyInfo;
+		private string instrument;
+		private string swingStrength;
+		private string lookBackSources;
+		private string fileName;
 		protected override void OnStateChange()
 		
 		{
@@ -48,18 +54,25 @@ namespace NinjaTrader.NinjaScript.Strategies
 				Print(string.Format("STATE.SETDEFAULTS"));
 				Description									= @"AuroraBacktest";
 				Name										= "AuroraBacktestUnlocked";
-				barType                                     = "UndefMinuteBar";
 				Calculate									= Calculate.OnBarClose;
+				DaysToLoad                                  = 187;
 				EntriesPerDirection							= 1;
 				EntryHandling								= EntryHandling.AllEntries;
 				IsExitOnSessionCloseStrategy				= true;
 				ExitOnSessionCloseSeconds					= 30;
 				IsFillLimitOnTouch							= false;
 				labels                                      = "CurrentBar,Time,Open,High,Low,Close,TrendPlot,BarsToNextSignal,BarsFromPreviousSignal,SignalPattern,BuySignalStopLine,SellSignalStopLine,DotPrice,OpenPrice";
-				localDate                                   = DateTime.Now;
 				MaximumBarsLookBack							= MaximumBarsLookBack.Infinite;
 				OrderFillResolution							= OrderFillResolution.Standard;
-				pathCSV 			                        = NinjaTrader.Core.Globals.UserDataDir + localDate.ToString("yyyyMMddHH") + barType + "outputs.csv"; // Define the Path to our test file can add/remove localDate.ToString("yyyyMMddHH") from middle
+				localDate                                   = DateTime.Now;
+				instrument                                  = Instrument.FullName;
+				barType                                     = BarsPeriod.BarsPeriodType;
+				barValue                                    = BarsPeriod.Value;
+				swingStrength                               = "UndefSwingStrength";
+				lookBackSources                             = "lookBackSources";
+				strategyInfo                                = "Instrument.barType.SwingStrength.lookBackSources";
+				fileName                                    = localDate.ToString("yyyyMMddHH") + "outputs.csv"; //can add/remove localDate.ToString("yyyyMMddHH") from middle
+				pathCSV 			                        = NinjaTrader.Core.Globals.UserDataDir + fileName; // Define the Path to our test file 
 				Slippage									= 0;
 				StartBehavior								= StartBehavior.ImmediatelySubmit;
 				TimeInForce									= TimeInForce.Gtc;
@@ -104,71 +117,25 @@ namespace NinjaTrader.NinjaScript.Strategies
 			if (BarsInProgress != 0 || CurrentBar < BarsRequiredToTrade) 
 				return;
 
-/////////////Debugging/////////////
-
-			Print(string.Format("CurrentBar"));
-			Print(CurrentBar);
-			//indiTachAur.TrendPlot.GetValueAt(dataIndex)
-			Print(string.Format("indiTachAur.TrendPlot.ToString"));
-			Print(indiTachAur.TrendPlot.ToString());
-			Print(string.Format("indiTachAur.TrendPlot.Count"));
-			Print(indiTachAur.TrendPlot.Count);
-			//indiTachAur.BarsToNextSignal.GetValueAt(dataIndex)
-			Print(string.Format("indiTachAur.BarsToNextSignal.ToString"));
-			Print(indiTachAur.BarsToNextSignal.ToString());
-			Print(string.Format("indiTachAur.BarsToNextSignal.Count"));
-			Print(indiTachAur.BarsToNextSignal.Count);
-			//indiTachAur.BarsFromPreviousSignal.GetValueAt(dataIndex)
-			Print(string.Format("indiTachAur.BarsFromPreviousSignal.ToString"));
-			Print(indiTachAur.BarsFromPreviousSignal.ToString());
-			Print(string.Format("indiTachAur.BarsFromPreviousSignal.Count"));
-			Print(indiTachAur.BarsFromPreviousSignal.Count);
-			//indiTachAur.SignalPattern.GetValueAt(dataIndex)
-			Print(string.Format("indiTachAur.SignalPattern.ToString"));
-			Print(indiTachAur.SignalPattern.ToString());
-			Print(string.Format("indiTachAur.SignalPattern.Count"));
-			Print(indiTachAur.SignalPattern.Count);
-			//indiTachAur.BuySignalStopLine.GetValueAt(dataIndex)
-			Print(string.Format("indiTachAur.BuySignalStopLine.ToString"));
-			Print(indiTachAur.BuySignalStopLine.ToString());
-			Print(string.Format("indiTachAur.BuySignalStopLine.Count"));
-			Print(indiTachAur.BuySignalStopLine.Count);
-			//indiTachAur.SellSignalStopLine.GetValueAt(dataIndex)
-			Print(string.Format("indiTachAur.SellSignalStopLine.ToString"));
-			Print(indiTachAur.SellSignalStopLine.ToString());
-			Print(string.Format("indiTachAur.SellSignalStopLine.Count"));
-			Print(indiTachAur.SellSignalStopLine.Count);
-			//indiTachAur.DotPrice.GetValueAt(dataIndex)
-			Print(string.Format("indiTachAur.DotPrice.ToString"));
-			Print(indiTachAur.DotPrice.ToString());
-			Print(string.Format("indiTachAur.DotPrice.Count"));
-			Print(indiTachAur.DotPrice.Count);
-			//indiTachAur.OpenPrice.GetValueAt(dataIndex)
-			Print(string.Format("indiTachAur.OpenPrice.ToString"));
-			Print(indiTachAur.OpenPrice.ToString());
-			Print(string.Format("indiTachAur.OpenPrice.Count"));
-			Print(indiTachAur.OpenPrice.Count);
-				
-			Print(string.Format("indiTachAur.BarsToNextSignal.GetValueAt(CurrenBar)"));
-			Print(indiTachAur.BarsToNextSignal.GetValueAt(CurrentBar));
-
 ////////////Printing area////////////
 
-			dataIndex = CurrentBar;
+			dataIndex = CurrentBar -1;
 			ntStockData = CurrentBar + "," + Time[0] + "," + Open[0] + "," + High[0] + "," + Low[0] + "," + Close[0];
 			auroraStockData = indiTachAur.TrendPlot.GetValueAt(dataIndex) + "," + indiTachAur.BarsToNextSignal.GetValueAt(dataIndex) + "," + indiTachAur.BarsFromPreviousSignal.GetValueAt(dataIndex) + "," + indiTachAur.SignalPattern.GetValueAt(dataIndex) + "," + indiTachAur.BuySignalStopLine.GetValueAt(dataIndex)+ "," + indiTachAur.SellSignalStopLine.GetValueAt(dataIndex) + "," + indiTachAur.DotPrice.GetValueAt(dataIndex) + "," + indiTachAur.OpenPrice.GetValueAt(dataIndex);
 			fullPrintOut = ntStockData + "," + auroraStockData;
 
-			Print(string.Format(fullPrintOut));	 
-
 			if (!File.Exists(pathCSV))
 			{
+				//localDate/value/instrument/value/barValue/value/barType/value/swingStrength/value/lookBackSettings/value/Enddate/value/lookbackDays
+				// strategyInfo = "localDate/"+localDate+"daysToLoad/"+DaysToLoad+"instrument/"+instrument+"barValue/"+barValue+"barType/"+barType;
 				sw = File.AppendText(pathCSV);
+				// sw.WriteLine(strategyInfo);
 				sw.WriteLine(labels);
 				sw.Close();
 			}
 			else
 			{
+				Print(string.Format(fullPrintOut));
 				sw = File.AppendText(pathCSV);
 				sw.WriteLine(fullPrintOut); // Append a new line to the file		
 				sw.Close(); // Close the file to allow future calls to access the file again.   
@@ -177,3 +144,51 @@ namespace NinjaTrader.NinjaScript.Strategies
 		}
 	}
 }
+
+/////////////Debugging/////////////
+
+			// Print(string.Format("CurrentBar"));
+			// Print(CurrentBar);
+			// //indiTachAur.TrendPlot.GetValueAt(dataIndex)
+			// Print(string.Format("indiTachAur.TrendPlot.ToString"));
+			// Print(indiTachAur.TrendPlot.ToString());
+			// Print(string.Format("indiTachAur.TrendPlot.Count"));
+			// Print(indiTachAur.TrendPlot.Count);
+			// //indiTachAur.BarsToNextSignal.GetValueAt(dataIndex)
+			// Print(string.Format("indiTachAur.BarsToNextSignal.ToString"));
+			// Print(indiTachAur.BarsToNextSignal.ToString());
+			// Print(string.Format("indiTachAur.BarsToNextSignal.Count"));
+			// Print(indiTachAur.BarsToNextSignal.Count);
+			// //indiTachAur.BarsFromPreviousSignal.GetValueAt(dataIndex)
+			// Print(string.Format("indiTachAur.BarsFromPreviousSignal.ToString"));
+			// Print(indiTachAur.BarsFromPreviousSignal.ToString());
+			// Print(string.Format("indiTachAur.BarsFromPreviousSignal.Count"));
+			// Print(indiTachAur.BarsFromPreviousSignal.Count);
+			// //indiTachAur.SignalPattern.GetValueAt(dataIndex)
+			// Print(string.Format("indiTachAur.SignalPattern.ToString"));
+			// Print(indiTachAur.SignalPattern.ToString());
+			// Print(string.Format("indiTachAur.SignalPattern.Count"));
+			// Print(indiTachAur.SignalPattern.Count);
+			// //indiTachAur.BuySignalStopLine.GetValueAt(dataIndex)
+			// Print(string.Format("indiTachAur.BuySignalStopLine.ToString"));
+			// Print(indiTachAur.BuySignalStopLine.ToString());
+			// Print(string.Format("indiTachAur.BuySignalStopLine.Count"));
+			// Print(indiTachAur.BuySignalStopLine.Count);
+			// //indiTachAur.SellSignalStopLine.GetValueAt(dataIndex)
+			// Print(string.Format("indiTachAur.SellSignalStopLine.ToString"));
+			// Print(indiTachAur.SellSignalStopLine.ToString());
+			// Print(string.Format("indiTachAur.SellSignalStopLine.Count"));
+			// Print(indiTachAur.SellSignalStopLine.Count);
+			// //indiTachAur.DotPrice.GetValueAt(dataIndex)
+			// Print(string.Format("indiTachAur.DotPrice.ToString"));
+			// Print(indiTachAur.DotPrice.ToString());
+			// Print(string.Format("indiTachAur.DotPrice.Count"));
+			// Print(indiTachAur.DotPrice.Count);
+			// //indiTachAur.OpenPrice.GetValueAt(dataIndex)
+			// Print(string.Format("indiTachAur.OpenPrice.ToString"));
+			// Print(indiTachAur.OpenPrice.ToString());
+			// Print(string.Format("indiTachAur.OpenPrice.Count"));
+			// Print(indiTachAur.OpenPrice.Count);
+				
+			// Print(string.Format("indiTachAur.BarsToNextSignal.GetValueAt(CurrenBar)"));
+			// Print(indiTachAur.BarsToNextSignal.GetValueAt(CurrentBar));
